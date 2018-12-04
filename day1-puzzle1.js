f9fffcc8401df0827970894faef4ddf108dae3f4
jeanbaptisteassouad <jean.baptiste@assouad.com>
2018-12-04 17:06:42 +0100

    Update day1-puzzle1.js

--- day1-puzzle1.js
+++ day1-puzzle1.js
@@ -3,7 +3,7 @@
   
   const lines = content.split('\n').map(a=>parseInt(a)).slice(0,-1)
 
-  const ans = lines.reduce((val,acc)=>acc+val,0)
+  const ans = lines.reduce((acc,val)=>acc+val,0)
 
   return ans
 })()
